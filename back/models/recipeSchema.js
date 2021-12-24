9467939799561b31193ddc89b5dcb32659355515
garrigoose <gkats11@gmail.com>
2021-12-23 21:07:29 -0500

    working on seeds

--- back/models/recipeSchema.js
+++ back/models/recipeSchema.js
@@ -2,11 +2,12 @@ const { mongo } = require('mongoose')
 const mongoose = require('../db/connection.js')
 
 const recipeSchema = new mongoose.recipeSchema({
-    title: {type: String},
+    title: {type: String, required: true},
     description: {type: String},
-    ingredients: [{type: String}],
-    steps: [{type: String}],
+    ingredients: [{type: String, required: true}],
+    steps: [{type: String, required: true}],
     picture: {type: String},
+    tags: [{type: String}]
 })
 
 const Recipe = mongoose.model('Recipe, recipeSchema')
