23dac22a2dd25ff355b46e64b71960675754f4e3
garrigoose <gkats11@gmail.com>
2022-01-05 19:34:17 -0500

    fixed structure

---
+++ server.js
@@ -0,0 +1,20 @@
+// dependencies
+const express = require("express");
+const app = express();
+const recipeController = require("./controllers/recipeController");
+const cors = require("cors");
+const { render } = require("express/lib/response");
+const Logger = require("nodemon/lib/utils/log");
+
+// middleware
+app.use(express.json());
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+app.use("/recipes", recipeController);
+app.get("/", (req, res) => {
+  console.log("test");
+  res.render("index.ejs");
+});
+
+app.listen(3000, () => console.log(`We're making snacks on port 3000`));
